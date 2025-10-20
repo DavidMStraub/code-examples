e6d55392051c3c32ffc2083ba5ce70b92a55813b
David Straub <straub@protonmail.com>
2025-10-20 18:17:30 +0200

    Mitternachtsformel: verbesserte Version

--- vl3_mitternachtsformel.py
+++ vl3_mitternachtsformel.py
@@ -1,47 +1,33 @@
-"""Skript, das eine Funktion definiert, die die Lösungen der quadratischen
-Gleichung ax²+bx+c=0 ausgibt."""
-
+"""Skript, das die Lösungen der quadratischen Gleichung ax²+bx+c=0 bestimmt."""
 
 def mitternachtsformel(a, b, c):
-    """Funktion die die Lösungen der quadratischen Gleichung ax²+bx+c=0 ausgibt.
+    """Funktion, die die sungen der quadratischen Gleichung ax²+bx+c=0 bestimmt.
 
-    Eingabewerte: Parameter a, b, c (Float)
+    Argumente:
+    - a, b, c: Koeffizienten der quadratischen Gleichung ax²+bx+c=0
+
     Ausgabe: anzahl_lsg, x1, x2, wobei:
-    - anzahl_lsg: Anzahl der Lösungen (0, 1 oder 2)
-    - x1, x2: Ort der Nullstelle oder `None`
+    - anzahl_lsg: Anzahl der Lösungen
+    - x1: erste Nullstelle oder None, falls es keine Lösung gibt
+    - x2: zweite Nullstelle oder None, falls es eine oder keine Lösung gibt
     """
-    determinante = b**2 - 4 * a * c
-    if determinante < 0:
-        return 0, None, None  # 0 Lösungen
-    elif determinante == 0:
+    diskriminante = b**2 - 4 * a * c
+    if diskriminante < 0:
+        return 0, None, None
+    elif diskriminante == 0:
         x1 = -b / (2 * a)
         return 1, x1, None
-    # ab hier: nur falls determinante > 0
-    x1 = (-b + determinante**0.5) / (2 * a)
-    x2 = (-b - determinante**0.5) / (2 * a)
-    return 2, x1, x2  # 2 Lösungen
+    # Standardfall D > 0
+    x1 = (-b + diskriminante**0.5) / (2 * a)
+    x2 = (-b - diskriminante**0.5) / (2 * a)
+    return 2, x1, x2
 
 
-ergebnis = mitternachtsformel(a=1, b=1, c=1)
-print(f"a=1, b=1, c=1: Ergebnis ist {ergebnis}")
-if ergebnis != (0, None, None):
-    print("Fehler! Unerwartetes Ergebnis")
+def test_mitternachtsformel(a, b, c):
+    ergebnis = mitternachtsformel(a, b, c)
+    print(f"Ergebnis für a={a}, b={b}, c={c}: {ergebnis}")
 
 
-ergebnis = mitternachtsformel(a=1, b=2, c=1)
-print(f"a=1, b=2, c=1: Ergebnis ist {ergebnis}")
-if ergebnis != (1, -1, None):
-    print("Fehler! Unerwartetes Ergebnis")
-
-
-ergebnis = mitternachtsformel(a=1, b=2, c=1)
-print(f"a=1, b=2, c=1: Ergebnis ist {ergebnis}")
-if ergebnis != (1, -1, None):
-    print("Fehler! Unerwartetes Ergebnis")
-
-
-ergebnis = mitternachtsformel(a=1, b=3, c=1)
-print(f"a=1, b=3, c=1: Ergebnis ist {ergebnis}")
-anzahl_lsg, x1, x2 = ergebnis
-print(f"x1: {x1:.5f}, erwartet: {-1.5 + 5**0.5 / 2:.5f}")
-print(f"x2: {x2:.5f}, erwartet: {-1.5 - 5**0.5 / 2:.5f}")
+test_mitternachtsformel(1, 1, 1)
+test_mitternachtsformel(1, 2, 1)
+test_mitternachtsformel(2, 5, 2)
